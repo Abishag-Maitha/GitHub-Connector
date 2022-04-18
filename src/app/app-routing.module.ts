6fed3097df3c450a0cd2ee5f30e19da43545b198
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-04-18 23:16:46 +0300

    create routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { profile } from 'console';
+import { ProfileComponent } from './profile/profile.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'profile', pathMatch:'full'},
+  {path:'profile', component:ProfileComponent},
+  {path:'repos', component:RepositoriesComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
