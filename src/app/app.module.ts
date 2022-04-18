e3c8b3dc6703b6fc2b9195fa576514254202253b
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-04-18 23:07:58 +0300

    Create Navbar, Profiles and Repositories components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    RepositoriesComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
